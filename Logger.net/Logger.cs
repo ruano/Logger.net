1653066012678bfdacac879aa91a51a7c94a0ab4
ruano <ruano.martinez@gmail.com>
2014-04-23 22:13:49 -0300

    Projeto logger.net

---
+++ Logger.net/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Logger.net
+{
+    public class Logger
+    {
+        private StreamWriter streamWriter;
+        private const string FileName = "dbDeployLogger.txt";
+        private readonly string path;
+
+        public Logger()
+        {
+            path = Path.GetTempPath();
+            CreateLogFile();
+        }
+
+        private StreamWriter CreateStream()
+        {
+            return new StreamWriter(Path.Combine(path, FileName), true);
+        }
+
+        private void CreateLogFile()
+        {
+            if (File.Exists(Path.Combine(path, FileName)))
+                return;
+
+            streamWriter = File.CreateText(Path.Combine(path, FileName));
+            streamWriter.Close();
+        }
+
+        public void Add(string comment, object value)
+        {
+            if (value == null)
+            {
+                throw new Exception("@Argument value cannot be null");
+            }
+
+            using (streamWriter = CreateStream())
+            {
+                streamWriter.WriteLine("Time and date: " + DateTime.Now.ToString("yyyy-MM-ddThh:mm:ss"));
+                streamWriter.WriteLine(comment + ":");
+                streamWriter.WriteLine(value.ToString());
+                streamWriter.WriteLine();
+            }
+        }
+    }
+}
---
+++ Logger.net/test/LoggerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+
+namespace Logger.net.test
+{
+    class LoggerTest
+    {
+        private Logger log;
+
+        [SetUp]
+        public void SetUp()
+        {
+            log = new Logger();
+        }
+
+        [Test]
+        public void create_log_file_once()
+        {
+            log.Add("Pass for here", "test");
+            var path = new FileInfo((Path.Combine(Path.GetTempPath(), "dbDeployLogger.txt")));
+            var file = File.ReadAllText(path.FullName);
+            Assert.IsTrue(file.Contains("Pass for here"));
+        }
+
+        [Test]
+        public void create_log_file_more()
+        {
+            for (int i = 0; i < 5; i++)
+                log.Add("Pass for here index: " + i, i);
+
+            var path = new FileInfo((Path.Combine(Path.GetTempPath(), "dbDeployLogger.txt")));
+            var file = File.ReadAllText(path.FullName);
+            Assert.IsTrue(file.Contains("Pass for here"));
+        }
+    }
+}
